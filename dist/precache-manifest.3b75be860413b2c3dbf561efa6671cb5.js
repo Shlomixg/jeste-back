51283643117389a7e1b99c43a9af8db07461e296
Itai Yosephi <itai.yosephi@gmail.com>
2018-08-02 13:25:01 +0300

    fixed dist

---
+++ dist/precache-manifest.3b75be860413b2c3dbf561efa6671cb5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0d2b7486b532288a980d0cd5d535340",
+    "url": "/img/chat-icon.e0d2b748.svg"
+  },
+  {
+    "revision": "543b692f35a12e241dc1513bf42b8743",
+    "url": "/img/main_bg.png"
+  },
+  {
+    "revision": "a1e634a8548ac66135ff32f190be1391",
+    "url": "/img/umitslider_help_l - Copy.png"
+  },
+  {
+    "revision": "b056efa8a306d7c04bc3",
+    "url": "/js/chunk-vendors.b056efa8.js"
+  },
+  {
+    "revision": "ddecc405413e919590c41d81a9fddec9",
+    "url": "/img/main_bg1.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c9f8442c28879fc881979512979a6180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2f7eec10f868caa0584",
+    "url": "/js/app.e2f7eec1.js"
+  },
+  {
+    "revision": "7718b3e3ff59dac728c55e725a3b0523",
+    "url": "/img/logo-no-bg.7718b3e3.svg"
+  },
+  {
+    "revision": "b6e6159efa7453d7abdc506ca5e1d377",
+    "url": "/img/file.b6e6159e.svg"
+  },
+  {
+    "revision": "6459849cb7031e486cd56958f0f1ae07",
+    "url": "/img/close.6459849c.svg"
+  },
+  {
+    "revision": "92a80167b42d2cbf2322e5f10a1512a0",
+    "url": "/icons8-handshake-heart-50.png"
+  },
+  {
+    "revision": "b056efa8a306d7c04bc3",
+    "url": "/css/chunk-vendors.e9e1c72c.css"
+  },
+  {
+    "revision": "e2f7eec10f868caa0584",
+    "url": "/css/app.17565547.css"
+  },
+  {
+    "revision": "fe4792d482196a50cf9ae0d9d90b6493",
+    "url": "/Copy.ico"
+  }
+];
